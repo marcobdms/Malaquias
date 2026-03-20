26aa0bbb779ef03550bf5b5a830dd6438f7871d3
marcobdms <el_marco4@hotmail.com>
2026-03-21 00:35:30 +0100

    lastest

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,10 +20,7 @@ app = FastAPI(title="Malaquías CV Screener")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "https://malaquias-liart.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
